2290e44c05faa46a2f43fcd53847ade21df43124
liiri <liri@apiiro.com>
2019-09-01 17:28:59 +0300

    fixing for desktop v mobile

--- src/App.js
+++ src/App.js
@@ -8,13 +8,21 @@ class App extends Component {
   };
 
   onClick = () => {
-    this.setState({hueDegrees: this.state.hueDegrees += Math.floor(Math.random() * 360)})
+    this.setState({hueDegrees: this.state.hueDegrees + Math.floor(Math.random() * 360)})
   };
 
   render() {
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     return (
-      <div className="container" style={{width: window.screen.width, height: window.screen.height}} onClick={this.onClick}>
-        <img className="img" src="https://i.imgur.com/TJisDte.png" style={{filter: `hue-rotate(${this.state.hueDegrees}deg)`}}/>
+      <div className="container" onClick={this.onClick}>
+        <img
+          alt='fingaz'
+          className="img"
+          src="https://i.imgur.com/TJisDte.png"
+          style={{
+            filter: `hue-rotate(${this.state.hueDegrees}deg)`,
+            paddingTop: (isMobile ? '25%' : 0)
+          }}/>
       </div>
     );
   }
